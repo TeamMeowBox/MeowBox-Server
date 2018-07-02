f63eb92b23a37c75c3958f6623b7bc6f503505b1
Kiyong <gy8971@gmail.com>
2018-07-02 21:26:23 +0900

    Add Oder Page

--- routes/order/order_routes.js
+++ routes/order/order_routes.js
@@ -8,6 +8,9 @@ const router = express.Router();
 // Signin
 // router.use('/main', require('./main'));
 
+// Order Page
+router.use('/order_page', require('./order_page'));
+
 
 
 module.exports = router;
