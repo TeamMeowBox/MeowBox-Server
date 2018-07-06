08623da4d8ad45588d78869c0513b226391e856a
kyoungin <kkmm114@gmail.com>
2018-07-06 21:32:54 +0900

    주서처리

--- routes/home/monthlyBox_detail.js
+++ routes/home/monthlyBox_detail.js
@@ -1,45 +1,45 @@
-const express = require('express');
-const router = express.Router();
+// // const express = require('express');
+// // const router = express.Router();
 
-const db = require('../../config/mongoPool.js')
-const mongoose = require('mongoose');
+// // const db = require('../../config/mongoPool.js')
+// // const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+// // const Schema = mongoose.Schema;
 
-var contentSchema = new Schema({
-    title : String,
-    main_img : String,
-    hashtag : [{type : String}],
-    main_text : String,
-    detail_img : [{type : String}],
-    detail_text : String
-})
+// // var contentSchema = new Schema({
+// //     title : String,
+// //     main_img : String,
+// //     hashtag : [{type : String}],
+// //     main_text : String,
+// //     detail_img : [{type : String}],
+// //     detail_text : String
+// // })
 
-var package = new Schema({
-    package_img : String,
-    package_date : String,
-    content : [{
-        title : String,
-        main_img : String,
-        hashtag : [{type : String}],
-        main_text : String,
-        detail_img : [{type : String}],
-        detail_text : String
-    }]
-},{ collection: 'testhomes'})
+// // var package = new Schema({
+// //     package_img : String,
+// //     package_date : String,
+// //     content : [{
+// //         title : String,
+// //         main_img : String,
+// //         hashtag : [{type : String}],
+// //         main_text : String,
+// //         detail_img : [{type : String}],
+// //         detail_text : String
+// //     }]
+// // },{ collection: 'testhomes'})
 
-var testhomes = mongoose.model('testhomes',package)
+// // var testhomes = mongoose.model('testhomes',package)
 
-router.get('/',async (req,res,next) => {
-    // let {id} = req.params
-    let result;
-    try{
-        result = await testhomes.find({});
-    } catch(err){
-        return next(err);
-    }
-    console.log(result)
-    return res.r(result);
-})
+// // router.get('/',async (req,res,next) => {
+// //     // let {id} = req.params
+// //     let result;
+// //     try{
+// //         result = await testhomes.find({});
+// //     } catch(err){
+// //         return next(err);
+// //     }
+// //     console.log(result)
+// //     return res.r(result);
+// // })
 
-module.exports = router;
+// module.exports = router;
