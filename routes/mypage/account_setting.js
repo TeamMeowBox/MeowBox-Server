9f83b7856e1fc71cead1139e9ba8d9ff54471332
Kiyong <gy8971@gmail.com>
2018-07-07 17:32:17 +0000

    main,account_setting

--- routes/mypage/account_setting.js
+++ routes/mypage/account_setting.js
@@ -36,9 +36,8 @@ router.get('/account/:user_idx', async (req, res, next) => {
         `
         SELECT users.name AS user_name, users.email, users.phone_number, users.image_profile, image_background,
                cats.name AS cat_name, cats.size, cats.birthday, cats.caution 
-        FROM users JOIN cats ON users.idx = cats.user_idx
-        WHERE users.idx = cats.user_idx
-        AND users.idx = ?
+        FROM users LEFT JOIN cats ON users.idx = cats.user_idx
+        WHERE users.idx = ?
         `;
     try {
         result = await db.Query(accountSelectQuery, [user_idx]);
