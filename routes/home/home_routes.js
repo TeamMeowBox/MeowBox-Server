5c2090278256f15c71d2a366c72e88698b2bdd2e
Kiyong <gy8971@gmail.com>
2018-07-07 14:56:24 +0900

    Fix MongoDB API

--- routes/home/home_routes.js
+++ routes/home/home_routes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 // Signin
 // router.use('/main', require('./main'));
 //router.get('/',(req,res)=>{  res("OK"); })  //몽고 디비 깔기전까지 라우터연결시킬 경로(나중에 지움) -경인 
-//  router.use('/monthlyBox_detail', require('./monthlyBox_detail'));
+router.use('/monthlyBox_detail', require('./monthlyBox_detail'));
 
 module.exports = router;
