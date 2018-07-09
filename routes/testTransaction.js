5fdc77749e68107c98573f611dda25311528a481
Gobukgol <zxczc159@gmail.com>
2018-07-09 18:10:37 +0900

    transaction response add & order_detail response modify

--- routes/testTransaction.js
+++ routes/testTransaction.js
@@ -8,22 +8,25 @@ router.get('/',async (req,res,next) => {
     let updateQuery1 = 
     `
     UPDATE test
-    SET test = ?, test2 = ?
+    SET test1 = ?, test2 = ?
     WHERE test3 = ?
     `
     
     let insertQuery2 =
     `
-    INSERT INTO test (test1,test2,test3)
+    INSERT INTO test (test,test2,test3)
     VALUES (?,?,?)
     `
 
     db.Transaction(async (connection) => {
         console.log("query1")
-        await connection.query(insertQuery2,["z","x","c"])
+        await connection.query(insertQuery2,["zasd","x","gtg"])
         console.log("query2")
-        await connection.query(updateQuery1,["ac","2v","6"])
+        await connection.query(updateQuery1,["avvv","2v","6"])
         console.log("end")
+        res.r()
+    }).catch(err => {
+        next(err)
     })
 })
 
