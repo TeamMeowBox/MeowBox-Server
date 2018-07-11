30221250206990153c79220b57a9e10dbaea1edf
Kiyong <gy8971@gmail.com>
2018-07-12 07:50:48 +0900

    iOS Pay Test

--- routes/order/order_result.js
+++ routes/order/order_result.js
@@ -7,88 +7,87 @@ const jwt = require('../../module/jwt');
 const axios = require('axios')
 const request = require('request');
 
+const pool = require('../../config/dbPool');
+
+var token = "90f0c2671d03ac252688344d34b18cb75dbbbe81"
+// var token = "cecb2d95f1f918d9746458a36105148fca1df3a4"
+
  
-    function updateClient(postData){
-            var clientServerOptions = {
-                // uri: 'http://'+clientHost+''+clientContext,
-                uri: 'https://api.iamport.kr/payments/prepare/305?_token=2957e399927521c8ce899594f62bdcdfffeb5e3a',
-                // body: JSON.stringify(postData),
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
+function preOrder(merchant_uid, amount){ // code  0 : success / 1 : fail
+    return new Promise(function(resolve, reject){
+        pool.getConnection(function(err, connection){
+            if(err) reject(err);
+            else {
+                // Set the headers
+                var headers = {
+                    'Content-Type':     'application/json'
                 }
+                // Configure the request
+                var options = {
+                    url: `https://api.iamport.kr/payments/prepare?_token=${token}`,
+                    method: 'POST',
+                    headers: headers,
+                    form: {'merchant_uid': merchant_uid, 'amount': amount }
+                }
+
+                // Start the request
+                request(options, function (err, response, body) {
+                    connection.release();
+                    if( err ){
+                        reject(err);
+                    }
+                    else if (response.statusCode == 200) {
+                        resolve(body)
+                    }
+                }) // end of request              
             }
-            request(clientServerOptions, function (error, response) {
-                console.log(error,response.body);
-                return;
-            });
-        }
-
-
-    function updateClient(postData){
-        var clientServerOptions = {
-            // uri: 'http://'+clientHost+''+clientContext,
-            // https://api.iamport.kr/payments/prepare?_token=2957e399927521c8ce899594f62bdcdfffeb5e3a
-            uri: 'https://api.iamport.kr/payments/prepare/305?_token=2957e399927521c8ce899594f62bdcdfffeb5e3a',
-            // body: JSON.stringify(postData),
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-        request(clientServerOptions, function (error, response) {
-            console.log(error,response.body);
-            return;
         });
-    }
+    }); // end of Promise
+}; // end of preOrder ()
 
+    
+function afterOrder(merchant_uid){ // code   0 : success /   -1 : fail
+    return new Promise(function(resolve, reject){
+        pool.getConnection(function(err, connection){
+            if(err) reject(err);
+            else {
+                var call = {
+                    uri: 'https://api.iamport.kr/payments/prepare/'+ merchant_uid + '?_token=' + `${token}`,
+                    method: 'GET'
+                }
 
-
-    function test(){
-
-        console.log('123123123123123123');
-        var clientServerOptions = {
-            // uri: 'http://'+clientHost+''+clientContext,
-            uri: `https://api.iamport.kr/payments/${imp_uid}`,
-            // body: JSON.stringify(postData),
-            method: 'GET',
-            headers: { "Authorization": "Bearer " + access_token } // 인증 토큰 Authorization header에 추가
-        }
-        request(clientServerOptions, function (error, response) {
-            console.log(error,response.body);
-            return;
+                request(call, function (err, response) {
+                    connection.release();
+                    if( err ){
+                        reject(err);
+                    }
+                    else if (response.statusCode == 200) {
+                        resolve(response.body)
+                    }
+                }); // end of request
+            } // end of else
         });
-    }
-
-
-
-
-        
-
-
+    }); // end of Promise
+}; // end of afterOrder ()
 
 
 
 // Written By 신기용
 router.get('/', async (req, res, next) => {
     try {
-        test();
         let imp_uid = req.query.imp_uid;
         let imp_success = req.query.imp_success;
         let merchant_uid = req.query.merchant_uid; 
+
+        if(! imp_success ){
+            return res.render('order_fail');
+        }
 
         console.log('imp_uid : ' + imp_uid);
         console.log('imp_success : ' + imp_success);
         console.log('merchant_uid : ' + merchant_uid);
 
-
-        return res.render('order_success');
-
-    
-
-
-        
-        console.log(' Before Access token ');
+        console.log(' To Access token ');
         // 액세스 토큰(access token) 발급 받기
         getToken = await axios({
             url: "https://api.iamport.kr/users/getToken",
@@ -99,128 +98,87 @@ router.get('/', async (req, res, next) => {
                 imp_secret: "1jtwZfvM7ouZLk2wHb74jw7LoZKOb1Acl4Jkjf7rDBqoVtVDE2IYzPKXOuL0wmcRrWcLbjcOVDdQ68tL" // REST API Secret
             }
         });
-        const { access_token } = getToken.data.response; // 인증 토큰
 
-        console.log('access_token : ' + access_token);
+        token = getToken.data.response.access_token;
 
-
-        
-
-        console.log(' Before Get getPaymentData ');
-        // imp_uid로 아임포트 서버에서 결제 정보 조회
-        const getPaymentData = await axios({
-            url: `https://api.iamport.kr/payments/${imp_uid}`, // imp_uid 전달
-            method: "get", // GET method
-            headers: { "Authorization": "Bearer " + access_token } // 인증 토큰 Authorization header에 추가
-        });
-        const paymentData = getPaymentData.data.response; // 조회한 결제 정보
-        console.log('paymentData : ' + paymentData);
-        
+        console.log('token : ' + token);
 
 
-        /*
-        console.log('here ?');
-
-        let preOrder = await axios({
-            url: "https://api.iamport.kr/payments/prepare?_token=61128c0a4e13aa8254ab45838fdec0ec92c3388b",
-            method: "post", // POST method
-            headers: { "Content-Type": "application/json" }, // "Content-Type": "application/json"
-            body: {
-                merchant_uid : "306",
-                amount : 100
-            }
-        })
-        console.log('preOrder : ' +  preOrder);
-        */
-
-        
-
-        /*
-        https://api.iamport.kr/payments/prepare?_token=61128c0a4e13aa8254ab45838fdec0ec92c3388b
-        body
-        merchant_uid
-        amount
-        */
-
-
-
-        /*
-
-       let afterOrder = await axios({
-            url: "https://api.iamport.kr/payments/prepare/305?_token=61128c0a4e13aa8254ab45838fdec0ec92c3388b",
-            method: "get" // GET method
-        })
-        console.log('afterOrder : ' +  afterOrder);
-
-        
-        */
-
-         
-
-        let selectAmountQuery=
+        let getAmountQuery = 
         `
-        SELECT price
+        SELECT user_idx, price
         FROM orders
-        WHERE idx = ?
+        WHERE idx = ? 
         `
-        
 
-        //  const order = await Orders.findById(paymentData.merchant_uid);
-        //  const amountToBePaid = order.amount; // 결제 되어야 하는 금액
+        let getAmountResult = await db.Query(getAmountQuery,[merchant_uid]);
 
-        // DB에서 결제되어야 하는 금액 조회
-         const amountToBePaid = await db.Query(selectAmountQuery,[merchant_uid]);
+        let preOrderResult = await preOrder(merchant_uid, getAmountQuery[0].price);
+        preOrderResult = JSON.parse(preOrderResult);
+        console.log('preOrderResult.code : ' + preOrderResult.code);
 
-        console.log('amountToBePaid : ' + amountToBePaid[0].price);
- 
-         // 결제 검증하기
-         const { amount, status } = paymentData;
-         console.log('amount : ' + amount);
-         if (amount === amountToBePaid[0].price){
-             console.log(' 3개 ===');
-         }
+        let afterOrderResult = await afterOrder(merchant_uid);
+        afterOrderResult = JSON.parse(afterOrderResult);
+        console.log('afterOrderResult.code : ' + afterOrderResult.code);
 
-         if (amount == amountToBePaid[0].price){
-            console.log(' 2개 ===');
+        if( afterOrderResult.code == 0) {
+            let orderResultInsertQuery =
+            `
+            INSERT INTO order_result(user_idx, imp_uid, merchant_uid )
+            VALUES(?,?,?)
+            `
+            await db.Query(orderResultInsertQuery, [getAmountResult[0].user_idx, imp_uid, merchant_uid]);
+
+            return res.render('order_success');
+        }
+        else {
+            return res.render('order_fail');
         }
 
-         if ( amount === amountToBePaid[0].price || amount === amountToBePaid[0].price) { // 결제 금액 일치. 결제 된 금액 === 결제 되어야 하는 금액
-            //  await Orders.findByIdAndUpdate(merchant_uid, { $set: paymentData }); // DB에 결제 정보 저장
-
-            console.log('status : ' + status);
- 
-             switch (status) {
-                 case "ready": // 가상계좌 발급
-                     // DB에 가상계좌 발급 정보 저장
-                     const { vbank_num, vbank_date, vbank_name } = paymentData;
-                     await Users.findByIdAndUpdate("/* 고객 id */", { $set: { vbank_num, vbank_date, vbank_name }});
-                     // 가상계좌 발급 안내 문자메시지 발송
-                     SMS.send({ text: `가상계좌 발급이 성공되었습니다. 계좌 정보 ${vbank_num} ${vbank_date} ${vbank_name}`});
-                     res.send({ status: "vbankIssued", message: "가상계좌 발급 성공" });
-                     break;
-                 case "paid": // 결제 완료
-                     res.send({ status: "success", message: "일반 결제 성공" });
-                     break;
-             }
-         } else { // 결제 금액 불일치. 위/변조 된 결제
-             throw { status: "forgery", message: "위조된 결제시도" };
-         }
-    } catch (e) {
-        res.status(400).send(e);
+    } catch(error){
+        return next(error);
     }
-
-    return res.render('order_success');
-
 });
 
 
 
 
+// Written By 신기용
+// APP 결제 유무 체크 
+router.post('/check_order', async (req, res, next) => {
+    const chkToken = jwt.verify(req.headers.authorization);
+    if (chkToken  == undefined) {
+        return next("10403"); // "description": "잘못된 인증 방식입니다.",
+    }
 
+    let {merchant_uid} = req.body;
 
+    let selectOrderHistoryQuery =
+    `
+    SELECT *
+    FROM order_result
+    WHERE user_idx = ? AND merchant_uid = ? AND used = FALSE;
+    `
 
+    let result = {};
+    try{
+        let selectOrderHistoryResult = await db.Query(selectOrderHistoryQuery,[chkToken.user_idx, merchant_uid]);
+        result.order_result = selectOrderHistoryResult.length == 0 ? false : true;
+        if( result.order_result ){
+            let updateUsedQuery = 
+            `
+            UPDATE order_result
+            SET used = TRUE;
+            WHERE user_idx = ?
+            `
+            await db.Query(updateUsedQuery,[chkToken.user_idx]);
+        }
+    }catch(error){
+        return next(error);
+    }
 
-
+    return res.r(result);
+});
 
 
 
@@ -268,48 +226,6 @@ router.get('/', async (req, res, next) => {
     // Web상으로 성공 이미지 보여준다
     return res.render('order_success');
 });
-
-
-// Written By 신기용
-// iOS 결제 유무 체크 
-router.post('/check_order', async (req, res, next) => {
-    const chkToken = jwt.verify(req.headers.authorization);
-    if (chkToken  == undefined) {
-        return next("10403"); // "description": "잘못된 인증 방식입니다.",
-    }
-
-    let {random_key} = req.body;
-
-    let selectOrderHistoryQuery =
-    `
-    SELECT *
-    FROM order_result
-    WHERE user_idx = ? AND random_key = ?
-    `
-
-    console.log('user_idx : ' + chkToken.user_idx );
-    console.log('random_key : ' + random_key);
-
-    let result = {};
-    try{
-        let selectOrderHistoryResult = await db.Query(selectOrderHistoryQuery,[chkToken.user_idx, random_key]);
-        result.order_result = selectOrderHistoryResult.length == 0 ? false : true;
-        if( result.order_result ){
-            let updateUsedQuery = 
-            `
-            UPDATE order_result
-            SET used = TRUE;
-            WHERE user_idx = ?
-            `
-            await db.Query(updateUsedQuery,[chkToken.user_idx]);
-        }
-    }catch(error){
-        return next(error);
-    }
-
-    return res.r(result);
-});
-
 
 
 
