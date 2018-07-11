ef942ccd2618ab33792e32369bac46ab318d9822
Kiyong <gy8971@gmail.com>
2018-07-12 00:03:10 +0900

    트랜잭션 처리 오타 해결

--- routes/mypage/account_setting.js
+++ routes/mypage/account_setting.js
@@ -295,7 +295,7 @@ router.post('/update_cat', async (req, res, next) => {
     
     //트랜잭션 처리
     db.Transaction(async (connection) => {
-        let result = await connection.Query(selectCatQuery, [chkToken.user_idx]);
+        let result = await connection.query(selectCatQuery, [chkToken.user_idx]);
         if( result.length == 0 ){
             return next(400)
         }
@@ -312,7 +312,8 @@ router.post('/update_cat', async (req, res, next) => {
             if( caution == undefined){
                 caution = result[0].caution;
             }
-            await connection.Query(updateQuery, [name,size,birthday,caution, chkToken.user_idx]);
+            await connection.query(updateQuery, [name,size,birthday,caution, chkToken.user_idx]);
+
         }
     }).catch(error => {
         return next(error)
