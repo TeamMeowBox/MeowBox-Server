07849ad7a3b8c1146c695b252e37789a417a2325
Kiyong <gy8971@gmail.com>
2018-07-13 18:23:54 +0900

    Update order Loic Add payment_flag

--- routes/order/order_page.js
+++ routes/order/order_page.js
@@ -197,7 +197,7 @@ router.get('/product/:product', async (req, res, next) => {
     let query = `
     select orders.product , reservations.*
     from reservations, orders
-    where orders.user_idx  = ? AND orders.idx = reservations.order_idx
+    where orders.user_idx  = ? AND orders.idx = reservations.order_idx AND orders.payment_flag = 1
     order by reservations.delivery_date desc;
     `
     try {
